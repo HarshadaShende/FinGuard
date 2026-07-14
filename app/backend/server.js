345216c9afd0a8800ccf4a0f42aa0ac1da09814e
HarshadaShende <shendeharshada@gmail.com>
2026-07-14 11:26:05 +0530

    refactor : seperate Express app from server startup

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,10 +1,5 @@
-const express = require("express");
-const app = express();
+const app = require("./src/app");
 const PORT = process.env.PORT || 3000;
-app.get("/", (req, res) => {
-    res.send("Welcome to FinGuard API")
-});
-
 app.listen(PORT, () => {
     console.log(`FinGuard Backend is running on port ${PORT}`)
 });
